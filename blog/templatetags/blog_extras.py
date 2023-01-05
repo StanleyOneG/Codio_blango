901f3e10fc2248e4e6cda6d3a89e592a2d764ea5
Stanislav <goncharuk.sm@yandex.ru>
2023-01-05 23:57:33 +0300

    In blog_extras.py escape and mark_safe replaced by format_html function

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,8 +1,7 @@
 
 from django import template
 from django.contrib.auth import get_user_model
-from django.utils.html import escape
-from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 
 user_model = get_user_model()
 
@@ -14,16 +13,15 @@ def author_details(author):
         return ""
 
     if author.first_name and author.last_name:
-        name = escape(f"{author.first_name} {author.last_name}")
+        name = f"{author.first_name} {author.last_name}"
     else:
-        name = escape(f"{author.username}")
+        name = f"{author.username}"
         
     if author.email:
-        email = escape(author.email)
-        prefix = f'<a href="mailto:{email}">'
-        sufix = f'</a>'
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        sufix = format_html('</a>')
     else:
         prefix = ""
         sufix = ""
         
-    return mark_safe(f"{prefix}{name}{sufix}")
+    return format_html('{}{}{}', prefix, name, sufix)
