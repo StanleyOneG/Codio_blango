313d21a2c023048beaa2ca832273bfdc540df47d
Stanislav <goncharuk.sm@yandex.ru>
2023-01-09 12:01:24 +0300

    blog_extras.py extended by inclusion tag with recent posts function

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,6 +2,7 @@
 from django import template
 from django.contrib.auth import get_user_model
 from django.utils.html import format_html
+from blog.models import Post
 
 user_model = get_user_model()
 
@@ -9,6 +10,7 @@ register = template.Library()
 
 @register.filter
 def author_details(author, current_user):
+    
     if not isinstance(author, user_model):
         return ""
 
@@ -44,3 +46,8 @@ def col(extra_classes=""):
 @register.simple_tag
 def endcol():
     return format_html('</div>')
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk = post.pk)[1:5]
+    return {'title': "Recent Posts", 'posts': posts}
