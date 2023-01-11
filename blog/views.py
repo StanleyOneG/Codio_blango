6540d2315aa48645ef324c5d7c196dfcaaa68c4d
Stanislav <goncharuk.sm@yandex.ru>
2023-01-11 16:13:24 +0300

    Finish database optimization

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,8 @@ logger = logging.getLogger(__name__)
 # Create your views here.
 def index(request):
     """ Generate a view for main page """
-    posts = Post.objects.filter(published_at__lte=timezone.now())
+    posts = (Post.objects.filter(published_at__lte=timezone.now())
+             .select_related('author'))
     logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
