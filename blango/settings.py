7091e365c3d0310ad7c89dbcb98e55023ae181ff
Stanislav <goncharuk.sm@yandex.ru>
2023-01-16 14:13:17 +0300

    Add Configuration value to Language Code

--- blango/settings.py
+++ blango/settings.py
@@ -146,7 +146,7 @@ class Dev(Configuration):
     # Internationalization
     # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-    LANGUAGE_CODE = 'en-us'
+    LANGUAGE_CODE = values.Value("en-us")
 
     # TIME_ZONE = 'UTC'
     TIME_ZONE = values.Value('UTC')
