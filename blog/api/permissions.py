e247d8beb704e31e2270b7e84fb3d1ea52df45ff
Stanislav <goncharuk.sm@yandex.ru>
2023-01-17 13:38:48 +0300

    Add custom permission class for post detail (only author can edit)

---
+++ blog/api/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+
+class AuthorModifyOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
+    def has_object_permission(self, request, view, obj):
+        if request.method == permissions.SAFE_METHODS:
+            return True
+        
+        return request.user == obj.author
